50989b766ace66c18c0440142ff67d05a13fe61e
Scooter Wadsworth <scooterwadsworth@gmail.com>
2012-08-13 16:41:46 -0600

    fix tiled layer with zoom offset

--- src/AGS.Layer.Tiled.js
+++ src/AGS.Layer.Tiled.js
@@ -1,13 +1,14 @@
 L.AGS.Layer.Tiled = L.TileLayer.extend({
   getTileUrl: function(tilePoint) {
-    var url = this._url + '/tile/{z}/{y}/{x}';
+    var url = this._url + '/tile/{z}/{y}/{x}',
+        zoom = this._getZoomForUrl();
 
     if ('offset' in this.options) {
       zoom = zoom + this.options.offset;
     }
 
     url = url.replace('{s}', '')
-             .replace('{z}', this._getZoomForUrl())
+             .replace('{z}', zoom)
              .replace('{x}', tilePoint.x)
              .replace('{y}', tilePoint.y);
 
